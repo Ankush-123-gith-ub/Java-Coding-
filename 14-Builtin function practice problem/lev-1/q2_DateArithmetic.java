e57c72cf81547e5d586699dc59d00787bd7fff39
Ankush <ffp900h@gmail.com>
2025-09-21 22:42:45 +0530

    [Ankush].add Builin funtion practice questions

---
+++ 14-Builtin function practice problem/lev-1/q2_DateArithmetic.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class q2_DateArithmetic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a date (yyyy-MM-dd): ");
+        String input = sc.nextLine();
+
+        LocalDate date = LocalDate.parse(input);
+        LocalDate result = date.plusDays(7).plusMonths(1).plusYears(2).minusWeeks(3);
+
+        System.out.println("Original Date: " + date);
+        System.out.println("Modified Date: " + result);
+    }
+}
