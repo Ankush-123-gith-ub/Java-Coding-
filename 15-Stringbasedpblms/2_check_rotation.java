fdbddb5145c5ddbe99fac1765c094d9b3acf3960
Ankush <ffp900h@gmail.com>
2025-09-22 14:37:32 +0530

    [Ankush].add StringQues2

---
+++ 15-Stringbasedpblms/2_check_rotation.java
@@ -0,0 +1,10 @@
+class check_rotation {
+    public static void main(String[] args){
+        String A = "ABCD";
+        String B = "CDAB";
+        char[] s = new char[A.length()];
+        s = A.toCharArray();
+        
+
+    } 
+}
