47b4368ceda5cfecd30c4feb6f2f0358ff2b37bb
Ankush <ffp900h@gmail.com>
2025-10-13 13:23:51 +0530

    [Ankush].add delete

--- Leetcode/8_strings_appears_substr.java
+++
@@ -1,9 +0,0 @@
-class string_appear{
-    public int numOfStrings(String[] patterns, String word) {
-        int sum = 0;
-        for(String s : patterns){
-            if(word.contains(s)) sum++;
-        }
-        return sum;
-    }
-}
