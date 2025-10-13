6fe1196801cc93d4cb2f97182278965e1c579414
Ankush <ffp900h@gmail.com>
2025-10-13 14:08:58 +0530

    [Ankush].delete

--- Leetcode/11_sum_of_squares_of_special_elements.java
+++
@@ -1,9 +0,0 @@
-class sum_of_squares_of_special_elements {
-    public int sumOfSquares(int[] nums) {
-        int sum = 0;
-        for(int i = 1 ; i <= nums.length ; i++ ){
-            if(nums.length % i == 0 ) sum+=Math.pow(nums[i-1],2);
-        }
-        return sum;
-    }
-}
--- Leetcode/434_number_ofseg_str.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int countSegments(String s) {
-        if(s == null) return 0;
-        s = s.trim();
-        if(s.isEmpty()) return 0;
-        String[] sb = s.trim().split("\\s+");
-        return sb.length;
-    }
-}
