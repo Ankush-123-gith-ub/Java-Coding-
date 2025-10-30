4d4999e8508df1ead9685f68fc3345247bb9f0f1
Ankush <ffp900h@gmail.com>
2025-10-30 23:31:17 +0530

    [Ankush].delete

--- Practice_simple_q/q12.java
+++
@@ -1,7 +0,0 @@
-class q12 {
-    public static void main(String args){
-    StringBuilder sb = new StringBuilder("amanaplanacanalapanama");
-    System.out.println(sb==sb.reverse());
-    }
-    
-}
