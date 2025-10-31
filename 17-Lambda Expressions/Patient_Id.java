ddfd0f2ec3ec7bb4561431d63113355f650ed1e7
Ankush <ffp900h@gmail.com>
2025-10-31 13:27:19 +0530

    [Ankush].delete main_content

--- 17-Lambda Expressions/Patient_Id.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-class hospital {
-    public static void main(String[] args) {
-        List<String> pid = Arrays.asList("P101", "P102", "P103");
-        pid.forEach(System.out::println);
-    }
-}
-
